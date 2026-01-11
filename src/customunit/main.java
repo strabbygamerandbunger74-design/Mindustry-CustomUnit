5be1200d6a5edee30a226b79a53125118269bb9d
meesst <meesst@example.com>
2026-01-11 19:45:18 +0800

    初始化项目 - 添加模组基本结构

---
+++ src/customunit/main.java
@@ -0,0 +1,24 @@
+package customunit;
+
+import arc.*;
+import arc.util.*;
+import mindustry.mod.*;
+
+public class main extends Mod {
+    
+    public main() {
+        Log.info("Loaded CustomUnitMod constructor.");
+    }
+    
+    @Override
+    public void init() {
+        Log.info("Initializing CustomUnitMod...");
+        // 初始化代码将在这里添加
+    }
+    
+    @Override
+    public void loadContent() {
+        Log.info("Loading CustomUnitMod content...");
+        // 加载内容代码将在这里添加
+    }
+}
